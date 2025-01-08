2e79af3f663dd18304b438e2b50b7e76d5b9b96a
Łukasz Tshipenchko <dev@zxc.sx>
2025-01-08 10:34:44 +0500

    Added common packages

--- plugins/common.nix
+++ plugins/common.nix
@@ -2,6 +2,30 @@
 {
   environment.systemPackages = with pkgs; [
     htop
+    neofetch
+    htop
+    micro
+    vim
+    curl
+    wget
+    axel
+    iproute2
+    ethtool
+    nettools
+    inetutils
+    git
+    python310Packages.speedtest-cli
+    ouch
+    file
+    tree
+    unzip
+    jq
+    moreutils
+    binutils
+    openssl
+    iotop
+    iftop
+    tmux
   ];
 }
 
