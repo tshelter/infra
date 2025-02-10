0ee1b4591f6a270afcc46c2fe0e23b70e9cb8314
Łukasz Tshipenchko <dev@zxc.sx>
2025-02-11 02:36:38 +0500

    Added monitoring

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
       modules = [
         ./hosts/inf/configuration.nix
         ./plugins/common.nix
+        ./plugins/monitoring.nix
         comin.nixosModules.comin
         ({...}: {
           services.comin = {
